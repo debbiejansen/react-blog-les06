4398965bed738950714142bf9d7ba995fd76f381
debbiejansen <debbiejansen94@hotmail.com>
2025-12-30 21:23:30 +0100

    eerste gedeelte van opdracht 2 gemaakt

--- src/components/Blogpost/Blogpost.jsx
+++ src/components/Blogpost/Blogpost.jsx
@@ -1,13 +1,22 @@
 import './Blogpost.css'
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import posts from '../../constants/data.json';
 
 function Blogpost() {
     const { id } = useParams();
 
+    const post = posts.find((item) => item.id === Number(id));
+
+    if (!post) {
+        return <p>Post not found</p>;
+    }
     return (
-        <div>Het productnummer is {id}</div>
-    )
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+        </div>
+    );
 }
 
 export default Blogpost
--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -1,12 +1,29 @@
 import './Overview.css'
-import React from "react";
-
+import React from 'react';
+import posts from '../../constants/data.json';
+import {Link} from 'react-router-dom';
 
 function Overview() {
     return (
-        <div className="page-container">
+        <div className="page-container-overview">
 
-            <h1>Overview test</h1>
+            {/*<h1>Overzicht ({posts.length} blogs)</h1>*/}
+            <ul className="blog-list">
+                {posts.map((post) => {
+                    return (
+                        <li key={post.id} className="blog-item">
+                            <h3 className="blog-name">
+                                <Link to={"/posts/" + post.id}>
+                                    {post.title} ({post.author})
+                                </Link>
+                            </h3>
+                            <p>
+                                {post.comments} reacties - {post.shares} keer gedeeld
+                            </p>
+                        </li>
+                    );
+                })}
+            </ul>
 
         </div>
     );
